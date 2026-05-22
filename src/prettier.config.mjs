00dda505f9fff3b1f9f73d586de4aad44a7159fe
G-Wulfer <genry.wulfer@yandex.ru>
2026-05-22 23:53:54 +0300

    first commit

---
+++ src/prettier.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import("prettier").Config} */
+export default {
+  trailingComma: "es5",
+  endOfLine: "lf",
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  arrowParens: "always",
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  importOrderCaseInsensitive: true,
+  importOrderParserPlugins: [
+    "classProperties",
+    "decorators-legacy",
+    "typescript"
+  ],
+  importOrder: ["<THIRD_PARTY_MODULES>", "^@/(.*)$", "^../(.*)", "^./(.*)"],
+  plugins: ["@trivago/prettier-plugin-sort-imports"]
+}
